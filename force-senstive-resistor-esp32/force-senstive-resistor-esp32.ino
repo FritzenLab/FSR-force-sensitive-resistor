1c92a99f019c8eacd734ded3412d013e632aecf1
Clovis Fritzen <clovisf@gmail.com>
2025-03-31 21:30:01 -0300

    added images

--- force-senstive-resistor-esp32/force-senstive-resistor-esp32.ino
+++ force-senstive-resistor-esp32/force-senstive-resistor-esp32.ino
@@ -1,49 +1,29 @@
-/*
-  Analog input, analog output, serial output
-
-  Reads an analog input pin, maps the result to a range from 0 to 255 and uses
-  the result to set the pulse width modulation (PWM) of an output pin.
-  Also prints the results to the Serial Monitor.
-
-  The circuit:
-  - potentiometer connected to analog pin 0.
-    Center pin of the potentiometer goes to the analog pin.
-    side pins of the potentiometer go to +5V and ground
-  - LED connected from digital pin 9 to ground through 220 ohm resistor
-
-  created 29 Dec. 2008
-  modified 9 Apr 2012
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  https://docs.arduino.cc/built-in-examples/analog/AnalogInOutSerial/
-*/
-
 // These constants won't change. They're used to give names to the pins used:
-const int analogInPin = 2;  // Analog input pin that the potentiometer is attached to
-
-
-int sensorValue = 0;  // value read from the pot
-
+const int analogInPin = 2; 
+int sensorValue = 0; 
+const int led = 8;
 
 void setup() {
-  // initialize serial communications at 9600 bps:
+  
   Serial.begin(115200);
+  pinMode(8, OUTPUT);
 }
 
 void loop() {
   // read the analog in value:
   sensorValue = analogRead(analogInPin);
-  // map it to the range of the analog out:
+ 
 
 
   // print the results to the Serial Monitor:
   Serial.print("sensor = ");
   Serial.println(sensorValue);
   
-
-  // wait 2 milliseconds before the next loop for the analog-to-digital
-  // converter to settle after the last reading:
+  if(sensorValue > 2400){
+    digitalWrite(led, LOW); // here should be HIGH, but logic in this ESP32-C3 Super mini is inverted
+  }else{
+    digitalWrite(led, HIGH);
+  }
+  
   delay(250);
 }
